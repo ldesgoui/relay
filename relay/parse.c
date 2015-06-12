4d7a318135c64f680c68b88a1693f4f61ea29960
Lucas DESGOUILLES <ldesgoui@e3r13p5.42.fr>
2015-06-12 19:23:30 +0200

    Could not compile, please check at home

--- relay/parse.c
+++ relay/parse.c
@@ -1,37 +1,72 @@
-/*
-** Relay.parse
-** ~~~~~~~~~~~
-**
-** This is a C extension due to the fact that I have more experience
-** manipulating strings with C.
-** It was also a good excuse to learn how to create a C module for Python.
-**
-** Copyright (c) 2015, ldesgoui <relay at ldesgoui dot xyz>
-** See LICENSE for more informations.
+/*!
+**! Relay.parse
+**! ~~~~~~~~~~~
+**!
+**! This is a C extension due to the fact that I have more experience
+**! manipulating strings with C.
+**! It was also a good excuse to learn how to create a C module for Python.
+**!
+**! Copyright (c) 2015, ldesgoui <relay at ldesgoui dot xyz>
+**! See LICENSE for more informations.
 */
 
 #include <Python.h>
 
-/*
-** parse.match(pattern: str, data: str) -> None | (tuple, dict)
-*/
+static int
+match(const char *pattern, const char *data,
+		PyObject *results, PyObject *mapped_results);
+
+static const char *parse_match_docstring =
+"Match the pattern from the first argument with the data from the second "
+"argument, both being strings. Returns a tuple of a list and a tuple."
+
 static PyObject *
 parse_match(PyObject *self, PyObject *args)
 {
 	const char		*pattern;
 	const char		*data;
-	PyObject		*r_args = PyList_New(0);
-	PyObject		*r_kwargs = PyDict_New();
-	int				i;
-	int				x;
-	const char		*key;
-	int				keylen;
-	const char		*value;
-	int				valuelen;
-
+	PyObject		*results;
+	PyObject		*mapped_results;
 
 	if (!PyArg_ParseTuple(args, "ss", &pattern, &data))
+	{
+		PyErr_SetString(PyExc_TypeError, "Arguments must be strings");
 		return Py_None;
+	}
+	results = PyList_New(0);
+	mapped_results = PyDict_New();
+	if (match(pattern, data, results, mapped_results) != 0)
+	{
+		PyErr_SetString(PyExc_ValueError, "Parsing went wrong");
+		// TODO: what happens in the case of an exception when both objects are initialized ?
+		// Do they leak?
+		return Py_None;
+	}
+	return Py_BuildValue("(OO)", PyList_AsTuple(results), mapped_results);
+	// TODO: would the AsTuple leak the original results ?
+}
+
+/*! Match:
+**! {name:format}
+**!
+**! Working:
+**! "hello {target}!" "hello world!" -> () {'target': 'world'}
+**! "hey {}" "hey :)" -> (":)") {}
+**!
+**! To be added:
+**! ":{sender:@'{nick}@{host}'} PRIVMSG {target} :{message}"  ":sup@example.com PRIVMSG #chan :sup"
+**! -> () {'sender': {'data': 'sup@example.com', 'nick': 'sup', 'host': 'example.com'}, 'target': '#chan', 'message': 'sup'}
+*/
+static int
+match(const char *pattern, const char *data,
+		PyObject *results, PyObject *mapped_results)
+{
+	int i;
+	int x;
+	const char *key;
+	int keylen;
+	const char *value;
+	int valuelen;
 
 	i = 0;
 	x = 0;
@@ -52,7 +87,7 @@ parse_match(PyObject *self, PyObject *args)
 				{
 					if (key[keylen] == '\0')
 					{
-						return Py_None;
+						return (1);
 					}
 					keylen++;
 				}
@@ -64,51 +99,56 @@ parse_match(PyObject *self, PyObject *args)
 			{
 				if (value[valuelen] == '\0')
 				{
-					return Py_None;
+					return (1);
 				}
 				valuelen++;
 			}
 			x += valuelen;
 			if (key == NULL)
 			{
-				PyList_Append(r_args, Py_BuildValue("s#", value, valuelen));
+				PyList_Append(results, Py_BuildValue("s#", value, valuelen));
 			}
 			else
 			{
-				PyDict_SetItem(r_kwargs, Py_BuildValue("s#", key, keylen),
+				PyDict_SetItem(mapped_results, Py_BuildValue("s#", key, keylen),
 						Py_BuildValue("s#", value, valuelen));
 			}
 		}
 		else if (pattern[i] != data[x])
 		{
-			return Py_None;
+			return (1);
 		}
 		i++;
 		x++;
 	}
-	return Py_BuildValue("(OO)", r_args, r_kwargs);
+	return (0);
 }
 
-//static char *ParseDocstring = "Relay.parse\n~~~~~~~~~~~\n\n"
-//"This is a C extension due to the fact that I have more experience\n"
-//"manipulating strings with C.\n"
-//"It was also a good excuse to learn how to create a C module for Python.\n\n"
-//"Copyright (c) 2015, ldesgoui <relay at ldesgoui dot xyz>\n"
-//"See LICENSE for more informations.";
 
-static PyMethodDef ParseMethods[] = {
-	{"match", parse_match, METH_VARARGS, "Match the pattern from the first "
-		"argument with the data from the second argument. "
-			"Returns either None or a tuple and a dict in a tuple."},
+/*
+** Module Initialization
+*/
+
+static const char *parse_module_docstring =
+"Relay.parse\n"
+"~~~~~~~~~~~\n\n"
+"This is a C extension due to the fact that I have more experience"
+"manipulating strings with C.\n"
+"It was also a good excuse to learn how to create a C module for Python.\n\n"
+"Copyright (c) 2015, ldesgoui <relay at ldesgoui dot xyz>\n"
+"See LICENSE for more informations.";
+
+static PyMethodDef parse_methods[] = {
+	{"match", parse_match, METH_VARARGS, parse_match_docstring,},
 	{NULL, NULL, 0, NULL}
 };
 
-static struct PyModuleDef parsemodule = {
+static struct PyModuleDef parse_module = {
 	PyModuleDef_HEAD_INIT,
 	"parse",
-	NULL,
+	parse_module_docstring,
 	-1,
-	ParseMethods,
+	parse_methods,
 };
 
 PyMODINIT_FUNC
